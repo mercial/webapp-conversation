36f227f8503b3522b32ceefa5ea5e9419b9c60d5
Vladislav <mercial13@gmail.com>
2023-09-08 10:03:54 +0600

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = 'e1acb5fd-4e07-4925-b8f4-6402546598ef'
+export const API_KEY = 'app-WtCOafHKXmnsig8IwTDg6PWY'
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
